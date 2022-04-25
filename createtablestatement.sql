bc907176b00c0e0b2df0879586c5b56dac269478
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-25 11:29:19 +0800

    Delete createtablestatement.sql

--- createtablestatement.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `datapoint` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `lat` float(10,6) NOT NULL,
-  `lng` float(10,6) NOT NULL,
-  `type` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
